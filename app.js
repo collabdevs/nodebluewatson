61003515d1973e34e46c638e22aa3fa90ed87e47
Daniel Medeiros <danielmmf@gmail.com>
2017-04-12 19:49:23 -0300

    porta

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World');
-}).listen(9090);
-console.log('rodando na porta 9090');
+}).listen(6001);
+console.log('rodando na porta 6001');
